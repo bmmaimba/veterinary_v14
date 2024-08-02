b96df0efc09a4a5e902c59d3835c12404b66e3f9
bmmaimba <48985103+bmmaimba@users.noreply.github.com>
2024-08-03 01:43:20 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -17,9 +17,12 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    'auto_install': False,
+    'installable': True,
+    'application': True,
     'category': 'Veterinary',
     'version': '14.4.6',
-
+    'license': 'LGPL-3',
     # any module necessary for this one to work correctly
     'depends': ['base','calendar','account','sale','base_automation'],
 
